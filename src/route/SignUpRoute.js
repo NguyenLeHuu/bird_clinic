01e4db4f5b65231c56fea7c9198fad75af09c477
NguyenLeHuu <huunlse150800@fpt.edu.vn>
2023-09-10 17:30:37 +0700

    .

--- src/route/SignUpRoute.js
+++
@@ -1,8 +0,0 @@
-const promiseRouter = require("express-promise-router");
-const SignUpController = require("../controllers/SignUpController");
-
-
-let route =  promiseRouter();
-route.post("/",SignUpController.SignUp);
-
-module.exports = route;
