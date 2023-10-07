af8c6ebd6733d65048a7ed931d9ec12c4718e3f9
NguyenLeHuu <huunlse150800@fpt.edu.vn>
2023-10-07 10:22:08 +0700

    madia tao lai

---
+++ src/services/MediaService.js
@@ -0,0 +1,106 @@
+const { Op } = require("sequelize");
+const db = require("../models/index");
+const crypto = require("crypto");
+
+let getAll = (type, type_id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let data = await db.Media.findAll({
+        where: {
+          [Op.and]: [{ type: type }, { type_id: type_id }],
+        },
+      });
+      resolve(data);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let getOne = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      // let data = await db.Media.findByPk(id);
+      let data = await db.Media.findOne({
+        where: {
+          media_id: id,
+        },
+      });
+      resolve(data);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let createMedia = (data, url) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const id = crypto.randomBytes(15).toString("hex");
+      const result = await db.Media.create({
+        media_id: id,
+        type: data.type,
+        type_id: data.type_id,
+        link: data.link,
+        is_before: data.is_before,
+        is_after: data.is_after,
+        type_service: data.type_service,
+        status: 1,
+      });
+      resolve(result);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let updateMedia = (id, name, quantity, price, mainimg, detail) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let data = await db.Media.update(
+        {
+          name: name,
+          quantity: quantity,
+          price: price,
+          detail: detail,
+        },
+        {
+          where: {
+            media_id: id,
+          },
+        }
+      );
+      resolve(data);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+let deleteMedia = (id) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      let data = await db.Media.update(
+        {
+          status: 0,
+        },
+        {
+          where: {
+            media_id: id,
+          },
+        }
+      );
+      resolve(data);
+    } catch (e) {
+      reject(e);
+    }
+  });
+};
+
+module.exports = {
+  getAll: getAll,
+  getOne: getOne,
+  createMedia: createMedia,
+  updateMedia: updateMedia,
+  deleteMedia: deleteMedia,
+};
