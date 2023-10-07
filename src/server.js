5078fd844681f5aee55be0e0602c57dcf0215c85
NguyenLeHuu <huunlse150800@fpt.edu.vn>
2023-10-07 08:23:01 +0700

    ...

--- src/server.js
+++ src/server.js
@@ -36,7 +36,7 @@ paypal.configure({
 
 app.use("/", route);
 
-let port = process.env.PORT || 3000; // use process.env to get value from .env
+let port = process.env.PORT || 3001; // use process.env to get value from .env
 
 app.use("/api-docs", swaggerUI.serve, swaggerUI.setup(swaggerFile));
 
@@ -52,29 +52,25 @@ const useHttps = process.env.HTTPS || false;
 
 let certPath = process.env.CERT_PATH;
 
-try {
-  if (useHttps === "true") {
-    https.createServer(
-      // Provide the private and public key to the server by reading each
-      // file's content with the readFileSync() method.
-      {
-        key: fs.readFileSync(`${certPath}/private.key`),
-        cert: fs.readFileSync(`${certPath}/certificate.crt`),
-        ca: fs.readFileSync(`${certPath}/ca_bundle.crt`),
-      },
-      app
+if (useHttps === "true") {
+  https.createServer(
+    // Provide the private and public key to the server by reading each
+    // file's content with the readFileSync() method.
+    {
+      key: fs.readFileSync(`${certPath}/private.key`),
+      cert: fs.readFileSync(`${certPath}/certificate.crt`),
+      ca: fs.readFileSync(`${certPath}/ca_bundle.crt`),
+    },
+    app
+  );
+  listen(port, () => {
+    console.log(
+      // `Server start port https://ec2-54-169-148-196.ap-southeast-1.compute.amazonaws.com:${port}`
+      `Server start port https://clinicsystem.io.vn:${port}`
     );
-    listen(port, () => {
-      console.log(
-        // `Server start port https://ec2-54-169-148-196.ap-southeast-1.compute.amazonaws.com:${port}`
-        `Server start port https://clinicsystem.io.vn:${port}`
-      );
-    });
-  } else {
-    app.listen(port, () => {
-      console.log(`Server start port http://localhost:${port}`);
-    });
-  }
-} catch (error) {
-  console.log("err start server");
+  });
+} else {
+  app.listen(port, () => {
+    console.log(`Server start port http://localhost:${port}`);
+  });
 }
